17ae7a517454adf2e74060715a18bd75372e6bbb
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-25 08:02:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let first = document.querySelector('input[name="fname"]')
 let last = document.querySelector('input[name="lname"]')
 function getFormvalue() {
-	function completeName(first,last){
-		return `${first} ${last}`
-	}
-	alert(first.value,last.value);
-}
+  alert(`${first.value} ${last.value}`)
+} 
